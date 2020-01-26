7d829e308d1c49b24c41dc13b0cdb58e71c82c59
Niclas <nxclxs.rist@gmail.com>
2020-01-26 03:33:24 +0100

    finish

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// TODO: JavaScript Part
-
 let months = document.querySelector('h1'),
 	weeks = document.querySelector('h2'),
 	days = document.querySelector('h3'),
@@ -7,34 +5,64 @@ let months = document.querySelector('h1'),
 	minutes = document.querySelector('h5'),
 	seconds = document.querySelector('h6');
 
-let salvation = new Date(2020, 04, 05);
+let d1 = Date.now();
+let d2 = new Date(2020, 04, 05);
 
-let sec = Math.floor((salvation - Date.now()) / 1000),
-	min = Math.floor(sec / 60),
-	hour = Math.floor(min / 60),
-	day = Math.floor(hour / 24),
-	week = Math.floor(day / 7),
-	month = (day / 30).toFixed(1);
+const secondsDiff = (d1, d2) => {
+	return Math.floor(Math.abs(d1 - d2) / 1000);
+};
 
-months.innerHTML = `${month} <span class='grey'>MONTHS</span>`;
-weeks.innerHTML = `${week} <span class='grey'>WEEKS</span>`;
-days.innerHTML = `${day} <span class='grey'>DAYS</span>`;
-hours.innerHTML = `${hour} <span class='grey'>HOURS</span>`;
-minutes.innerHTML = `${min} <span class='grey'>MINUTES</span>`;
-seconds.innerHTML = `${sec} <span class='grey'>SECONDS</span>`;
+const minutesDiff = (d1, d2) => {
+	return Math.floor(secondsDiff(d1, d2) / 60);
+};
+
+const hoursDiff = (d1, d2) => {
+	return Math.floor(minutesDiff(d1, d2) / 60);
+};
+
+const daysDiff = (d1, d2) => {
+	return Math.floor(hoursDiff(d1, d2) / 24);
+};
+
+const weeksDiff = (d1, d2) => {
+	return Math.floor(daysDiff(d1, d2) / 7);
+};
+
+const yearsDiff = (d1, d2) => {
+	return new Date(d1).getFullYear() - new Date(d2).getFullYear();
+};
+
+const monthsDiff = (d1, d2) => {
+	return (daysDiff(d1, d2) / 30).toFixed(1);
+};
+
+months.innerHTML = `${monthsDiff(d1, d2)} <span class='grey'>MONTHS</span>`;
+weeks.innerHTML = `${weeksDiff(d1, d2)} <span class='grey'>WEEKS</span>`;
+days.innerHTML = `${daysDiff(d1, d2)} <span class='grey'>DAYS</span>`;
+hours.innerHTML = `${hoursDiff(d1, d2)} <span class='grey'>HOURS</span>`;
+minutes.innerHTML = `${minutesDiff(d1, d2)} <span class='grey'>MINUTES</span>`;
+seconds.innerHTML = `${secondsDiff(d1, d2)} <span class='grey'>SECONDS</span>`;
 
 setInterval(() => {
-	sec = Math.floor((salvation - Date.now()) / 1000);
-	min = Math.floor(sec / 60);
-	hour = Math.floor(min / 60);
-	day = Math.floor(hour / 24);
-	week = Math.floor(day / 7);
-	month = (day / 30).toFixed(1);
-
-	months.innerHTML = `${month} <span class='grey'>MONTHS</span>`;
-	weeks.innerHTML = `${week} <span class='grey'>WEEKS</span>`;
-	days.innerHTML = `${day} <span class='grey'>DAYS</span>`;
-	hours.innerHTML = `${hour} <span class='grey'>HOURS</span>`;
-	minutes.innerHTML = `${min} <span class='grey'>MINUTES</span>`;
-	seconds.innerHTML = `${sec} <span class='grey'>SECONDS</span>`;
+	months.innerHTML = `${monthsDiff(
+		Date.now(),
+		d2
+	)} <span class='grey'>MONTHS</span>`;
+	weeks.innerHTML = `${weeksDiff(
+		Date.now(),
+		d2
+	)} <span class='grey'>WEEKS</span>`;
+	days.innerHTML = `${daysDiff(Date.now(), d2)} <span class='grey'>DAYS</span>`;
+	hours.innerHTML = `${hoursDiff(
+		Date.now(),
+		d2
+	)} <span class='grey'>HOURS</span>`;
+	minutes.innerHTML = `${minutesDiff(
+		Date.now(),
+		d2
+	)} <span class='grey'>MINUTES</span>`;
+	seconds.innerHTML = `${secondsDiff(
+		Date.now(),
+		d2
+	)} <span class='grey'>SECONDS</span>`;
 }, 1000);
